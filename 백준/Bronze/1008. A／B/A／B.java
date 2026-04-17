ff62f4deb1228af323d446b34d61101d071bfc4f
heekung <s2620@e-mirim.hs.kr>
2026-04-17 10:40:35 +0900

    [Bronze V] Title: A/B, Time: 176 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String arags[]){
+        Scanner scan=new Scanner(System.in);
+        double a = scan.nextInt();
+        double b = scan.nextInt();
+        System.out.print(a/b);
+    }
+}
