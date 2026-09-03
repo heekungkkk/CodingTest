ba0e6be3113f56e94fc201c5c4ed597e26970ddf
희경 <s2620@e-mirim.hs.kr>
2026-09-03 21:57:43 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 8.21 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(int i = 0; i<myString.length(); i++){
+            if(!(myString.charAt(i)>='A' && myString.charAt(i)<='Z')){
+                answer += (char)((myString.charAt(i))-32);
+            } else answer += myString.charAt(i);
+        }
+        return answer;
+    }
+}
